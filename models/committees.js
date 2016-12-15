6c0b63c0a4f0286f9cf31a329e1719decfb08e49
Matt Casey <matt@mattcasey.nyc>
2016-12-15 16:49:19 -0500

    hacky committee data added

--- models/committees.js
+++ models/committees.js
@@ -1,5 +1,3 @@
-const keystone = require('keystone');
-const Types = keystone.Field.Types;
 const committees = [
   {
     "id": "HSAG",
@@ -992,92 +990,5 @@ const committees = [
     "subcommittees": []
   }
 ]
-/**
- * User Model
- * ==========
- */
 
-const Task = new keystone.List('Task');
-
-const subcommitteeFields = {};
-
-committees.forEach((committee) => {
-  subcommitteeFields[committee.id + '_subcommittee'] = {
-    type: Types.Select,
-    label: 'subcommittee',
-    dependsOn: { 'templateProps.committee': committee.id },
-    options: committee.subcommittees.map(subcommittee => ({
-      value: subcommittee.id, label: subcommittee.name
-    }))
-  }
-})
-
-Task.add({
-	name: { type: Types.Text, required: true, index: true },
-  issue: { type: Types.Relationship, ref: 'Issue', initial: true, required: true },
-  tags: { type: Types.TextArray, required: false },
-  location: {
-    label: 'leave blank to use user\'s location',
-    type: Types.Location,
-    defaults: { country: 'us' },
-    improve: true,
-    enableMapsAPI: true,
-  },
-  duration: {
-    type: Types.Select,
-    required: true,
-    default: 30,
-    numeric: true,
-    options: [
-      { value: 30, label: '30 minutes' },
-      { value: 60, label: 'One hour' },
-      { value: 180, label: 'Half day' },
-      { value: 360, label: 'Full day' }
-    ]
-  },
-  startDate: { label: 'Start Date', type: Types.Date, default: Date.now },
-  endDate: { label: 'End Date', type: Types.Date },
-  template: {
-    type: Types.Select,
-    required: true,
-    default: 'CallCongress',
-    options: [
-      { value: 'CallCongress', label: 'Call your congressmen and congresswomen' },
-      { value: 'CallCommittee', label: 'Call a committee or subcommittee' },
-      { value: 'CallSenate', label: 'Call your senators' },
-      { value: 'CallHouse', label: 'Call your representatives' }
-    ]
-  },
-  templateProps: Object.assign({
-    committee: { type: Types.Select, dependsOn: { template: 'CallCommittee' }, options: committees.map(committee => ({
-      label: 'committee',
-      value: committee.id, label: committee.name
-    }))},
-  },
-  subcommitteeFields,
-  {
-    requestedAction: { label: 'Requested Action', type: String, dependsOn: { template: ['CallSenate', 'CallCommittee', 'CallHouse', 'CallCongress'] } },
-    petitionScript: { label: 'Petition Script', type: Types.Html, wysiwyg: true, dependsOn: { template: ['CallSenate', 'CallCommittee', 'CallHouse', 'CallCongress'] } },
-    thankYouScript: { label: 'Thank You Script', type: Types.Html, wysiwyg: true, dependsOn: { template: ['CallSenate', 'CallCommittee', 'CallHouse', 'CallCongress'] } }
-  })
-});
-
-// Provide access to Keystone
-// Task.schema.virtual('canAccessKeystone').get(function() {
-// 	return this.isAdmin;
-// });
-
-
-/**
- * Relationships
- */
-
-// Task.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
-
-
-/**
- * Registration
- */
-
-Task.defaultColumns = 'name, issue, template';
-Task.register();
+export default committees;
