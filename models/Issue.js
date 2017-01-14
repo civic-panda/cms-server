f0a7e5ce783f65e90c665c88680836330693325f
Matt Casey <matt@mattcasey.nyc>
2017-01-14 16:51:47 -0500

    reordered items

--- models/Issue.js
+++ models/Issue.js
@@ -10,12 +10,12 @@ const Issue = new keystone.List('Issue');
 
 Issue.add({
 	name: { type: Types.Text, required: true, index: true },
-  summary: { type: Types.Html, wysiwyg: true, initial: false, required: false },
-  facts: { label: 'Key Facts (bullets)', type: Types.Html, wysiwyg: true, initial: false, required: false },
-  reading: { label: 'Additional Reading (links)', type: Types.Html, wysiwyg: true, initial: false, required: false },
   brandColor: { abel: 'Brand Accent Color', type: Types.Color, initial: false, required: false },
   logo: { type: Types.CloudinaryImage, autoCleanup : true, initial: false, required: false },
-  image: { type: Types.CloudinaryImage, autoCleanup : true, initial: false, required: false }
+  image: { type: Types.CloudinaryImage, autoCleanup : true, initial: false, required: false },
+  summary: { type: Types.Html, wysiwyg: true, initial: false, required: false },
+  facts: { label: 'Key Facts (bullets)', type: Types.Html, wysiwyg: true, initial: false, required: false },
+  reading: { label: 'Additional Reading (links)', type: Types.Html, wysiwyg: true, initial: false, required: false }
 });
 
 /**
