39c3ad03cf36bb903d59f6877aad00ee037ebdc3
Matt Casey <matt@mattcasey.nyc>
2017-01-14 23:38:19 -0500

    hotfix

--- models/Issue.js
+++ models/Issue.js
@@ -10,7 +10,7 @@ const Issue = new keystone.List('Issue');
 
 Issue.add({
 	name: { type: Types.Text, required: true, index: true },
-	callToAction: { label: 'Call To Action', type: Types.Text, required: true, index: true },
+	callToAction: { label: 'Call To Action', type: Types.Text, required: false, index: true },
   brandColor: { label: 'Brand Accent Color', type: Types.Color, initial: false, required: false },
   logo: { type: Types.CloudinaryImage, autoCleanup : true, initial: false, required: false },
   image: { label: 'Hero Image', type: Types.CloudinaryImage, autoCleanup : true, initial: false, required: false },
